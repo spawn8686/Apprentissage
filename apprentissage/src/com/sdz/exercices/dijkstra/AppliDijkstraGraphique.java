d63ab4b0a71f3ff9e94e576c4c5e8366ab1062a9
Jeremy_Monac <bressoud1986@gmail.com>
2019-07-03 07:46:03 +0200

    correction text du titre de la JFrame dans class Main AppliDijkstraGraphque.java

--- apprentissage/src/com/sdz/exercices/dijkstra/AppliDijkstraGraphique.java
+++ apprentissage/src/com/sdz/exercices/dijkstra/AppliDijkstraGraphique.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 
 public class AppliDijkstraGraphique {
 	public static void main(String[] arg) throws IOException  {
-		JFrame fenetre = new JFrame("Plus cours chemins)");
+		JFrame fenetre = new JFrame("(Plus cours chemins)");
 	 	Arborescence dijkstra = new Arborescence(arg[0]);
 	 	dijkstra.marquerArbo();
 	 	fenetre.setContentPane(new PanneauDessin(dijkstra));
