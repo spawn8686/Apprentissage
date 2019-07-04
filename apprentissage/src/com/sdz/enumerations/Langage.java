08da5d21cb2e42a4426c1f030bed8551008437d6
Jeremy_Monac <bressoud1986@gmail.com>
2019-07-04 16:27:58 +0200

    Suite et fin de l'exercice sur les énumérateur

--- apprentissage/src/com/sdz/enumerations/Langage.java
+++ apprentissage/src/com/sdz/enumerations/Langage.java
@@ -2,25 +2,59 @@ package com.sdz.enumerations;
 
 enum Langage 
 {
-	JAVA("Langage JAVA"),
-	Pascal("Langage Pascal"),
-	Python("Langage Python"),
-	HTML("Langage HTML"),
-	CSS("Langage CSS"),
-	PHP("Langage PHP"),
-	JavaScript("Langage JavaScript"),
-	C("Langage C"),
-	CPlus("Langage CPlus"),
-	CSharp("Langage CSharp");
+	JAVA("Langage JAVA", "Eclipse"),
+	Pascal("Langage Pascal", "Lazarus"),
+	Python("Langage Python", "Atom"),
+	HTML("Langage HTML", "SublimText"),
+	CSS("Langage CSS", "SublimText"),
+	PHP("Langage PHP", "SublimText"),
+	JavaScript("Langage JavaScript", "SublimText"),
+	C("Langage C", "Code::Blocks"),
+	CPlus("Langage CPlus", "C++ Builder"),
+	CSharp("Langage CSharp", "Visual Studio");
 	
 	private String name = "";
+	private String editor = "";
 	
-	Langage(String name) 
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	//									CONSTRUCTEUR
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	Langage(String name, String editor) 
+	{
+		this.name = name;
+		this.editor = editor;
+	}
+	
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	//									GETTER
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	public String getName()
+	{
+		return this.name;
+	}
+	public String getEditor()
+	{
+		return this.editor;
+	}
+	
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	//									SETTER
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	public void setName(String name)
 	{
 		this.name = name;
 	}
+	public void setEditor(String editor)
+	{
+		this.editor = editor;
+	}
+		
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	//									METHODES
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	public String toString()
 	{
-		return name;
+		String str = "Pour le langage '"+this.name+"', on peut utiliser l'éditeur '"+this.editor+"'";
+		return str;
 	}
 }
--- apprentissage/src/com/sdz/enumerations/MainEnumeration.java
+++ apprentissage/src/com/sdz/enumerations/MainEnumeration.java
@@ -1,16 +1,41 @@
 package com.sdz.enumerations;
 
+import com.sdz.exercices.Titre;
+
 class MainEnumeration 
 {
 	public static void main(String[] args) 
 	{
+		Titre titre = new Titre();
+		
+		titre.titre("Test d'énumération des objets de la class enum");
 		for (Langage langue : Langage.values()) 
 		{
-			if (Langage.HTML.equals(langue)) 
-				System.out.println("J'aime le : "+langue);
+			if (Langage.JAVA.equals(langue)) 
+				System.out.println("------"+langue+"------");
 			else
 				System.out.println(langue);
 		}
+		
+		titre.titre("Test des méthodes de la class enum");
+		Langage l1 = Langage.CSS;
+		Langage l2 = Langage.JAVA;
+		
+		titre.titre("Contrôle des données avant modification", false);
+		System.out.println("l1 name = "+l1.getName());
+		System.out.println("l2 editor = "+l2.getEditor());
+		System.out.println(l1.toString());
+		System.out.println(l2.toString());
+		
+		titre.titre("Modification des données", false);		
+		l1.setName("CSS3");
+		l2.setEditor("SublimText");
+		
+		titre.titre("Contrôle des données après modification", false);
+		System.out.println("l1 name = "+l1.getName());
+		System.out.println("l2 editor = "+l2.getEditor());
+		System.out.println(l1.toString());
+		System.out.println(l2.toString());
 	}
 
 }
