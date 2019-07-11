ed8cfbfc5403320a6b7add18faaa6cb4b5bbb31c
Jeremy_Monac <bressoud1986@gmail.com>
2019-07-11 14:53:14 +0200

    Création d'un package euroMillion pour le calcul vectorielle du nombre de possiblité

---
+++ apprentissage/src/com/sdz/exercices/euroMillion/Factorielle.java
@@ -0,0 +1,40 @@
+package com.sdz.exercices.euroMillion;
+
+import java.math.BigInteger;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// 						Class pour le calcul d'une factorielle
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+class Factorielle 
+{
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	// 									VARIABLES
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	int facteur = 1;
+	
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	// 									CONSTRUCTEURS
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	protected Factorielle()
+	{}
+	
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	// 									METHODES
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	// retourne la factorielle du nombre entier entré en paramètre
+	protected BigInteger facteur(int fac)
+	{
+		this.facteur = fac;
+		int loop = 1;
+		BigInteger result = BigInteger.ONE;
+		
+		while (loop <= this.facteur) 
+		{
+			result = result.multiply(BigInteger.valueOf(loop));
+			loop++;
+		}
+		
+		return result;
+	}
+}
---
+++ apprentissage/src/com/sdz/exercices/euroMillion/Main.java
@@ -0,0 +1,27 @@
+package com.sdz.exercices.euroMillion;
+
+import java.math.BigInteger;
+
+class Main {
+
+	public static void main(String[] args) 
+	{
+		Factorielle facto = new Factorielle();
+	
+		BigInteger nbr1 = facto.facteur(50);
+		BigInteger nbr2 = facto.facteur(45);
+		BigInteger nbr3 = facto.facteur(5);
+		BigInteger nbr4 = facto.facteur(9);
+		BigInteger nbr5 = facto.facteur(7);
+		BigInteger nbr6 = facto.facteur(2);
+		
+		BigInteger C0 = nbr2.multiply(nbr3);
+		BigInteger C = nbr1.divide(C0);
+		BigInteger E0 = nbr5.multiply(nbr6);
+		BigInteger E = nbr4.divide(E0);
+		BigInteger R = C.multiply(E);
+		
+		System.out.println("le nombre de possibilité pour l'EuroMillion est de \""+R+"\"");
+	}
+
+}
