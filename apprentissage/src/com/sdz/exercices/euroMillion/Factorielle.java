5ce2fa648131ad45978cd915cd910e77833df1fe
Jeremy_Monac <bressoud1986@gmail.com>
2019-07-11 16:27:31 +0200

    Ajout de nouvelle méthode dans la class Factorielle du package euroMillion et test dans la méthode Main associée

--- apprentissage/src/com/sdz/exercices/euroMillion/Factorielle.java
+++ apprentissage/src/com/sdz/exercices/euroMillion/Factorielle.java
@@ -16,14 +16,14 @@ class Factorielle
 	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	// 									CONSTRUCTEURS
 	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-	protected Factorielle()
+	public Factorielle()
 	{}
 	
 	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	// 									METHODES
 	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	// retourne la factorielle du nombre entier entré en paramètre
-	protected BigInteger facteur(int fac)
+	public BigInteger facteur(int fac)
 	{
 		this.facteur = fac;
 		int loop = 1;
@@ -36,6 +36,26 @@ class Factorielle
 		}
 		
 		return result;
+	}
+	public BigInteger simpleCombinaison(int grille, int choix)
+	{
+		Factorielle facto = new Factorielle();
+		
+		BigInteger nbr1 = facto.facteur(grille);
+		BigInteger nbr2 = facto.facteur(grille - choix);
+		BigInteger nbr3 = facto.facteur(choix);
+		
+		BigInteger temp = nbr2.multiply(nbr3);
+		BigInteger result = nbr1.divide(temp);		
+		
+		return result;
+	}
+	public void fullCombinaison(String str, int grille1, int grille2, int choix1, int choix2)
+	{
+		Factorielle facto = new Factorielle();		
+		BigInteger res = facto.simpleCombinaison(grille1, choix1).multiply(facto.simpleCombinaison(grille2, choix2));
+		
+		System.out.println("Pour le jeux : '"+str+"', il y a un total de '"+res+"' possiblités");
 	}
 	
 	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--- apprentissage/src/com/sdz/exercices/euroMillion/Main.java
+++ apprentissage/src/com/sdz/exercices/euroMillion/Main.java
@@ -7,22 +7,15 @@ class Main {
 	public static void main(String[] args) 
 	{
 		Factorielle facto = new Factorielle();
-	
-		BigInteger nbr1 = facto.facteur(50);
-		BigInteger nbr2 = facto.facteur(45);
-		BigInteger nbr3 = facto.facteur(5);
-		BigInteger nbr4 = facto.facteur(9);
-		BigInteger nbr5 = facto.facteur(7);
-		BigInteger nbr6 = facto.facteur(2);
+		BigInteger res = facto.simpleCombinaison(50, 5).multiply(facto.simpleCombinaison(9, 2));
 		
-		BigInteger C0 = nbr2.multiply(nbr3);
-		BigInteger C = nbr1.divide(C0);
-		BigInteger E0 = nbr5.multiply(nbr6);
-		BigInteger E = nbr4.divide(E0);
-		BigInteger Result = C.multiply(E);
+		System.out.println("le nombre de possibilité pour l'EuroMillion, sans les étoiles, est de \""+facto.simpleCombinaison(50, 5)+"\"");
 		
-		System.out.println("le nombre de possibilité pour l'EuroMillion est de \""+Result+"\"");
-		System.out.println(facto.facteur(7));
+		System.out.println("le nombre de possibilité pour l'EuroMillion, uniquement des étoiles, est de \""+facto.simpleCombinaison(9, 2)+"\"");	
+		
+		System.out.println("le nombre TOTAL de possibilité pour l'EuroMillion est de \""+res+"\"");
+		
+		facto.fullCombinaison("EuroMillion", 50, 9, 5, 2);
 	}
 
 }
